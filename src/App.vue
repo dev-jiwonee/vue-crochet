d8031c5fcddd11353d20a06d9cf0bb2dcfeed183
jiwon <dev.jiwonee@gmail.com>
2026-03-26 19:34:45 +0900

    선택한 항목 상단에 노출

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import dayjs from "dayjs";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 // 작품명, 전체 단수
 const name = ref("");
 const total = ref(0);
+
+const selectedId = ref(null);
+const selectedItem = computed(() =>
+  crochets.value.find((c) => c.id === selectedId.value),
+);
 
 // 작품 저장 배열, 초기엔 빈 배열
 const crochets = ref(JSON.parse(localStorage.getItem("crochets")) || []);
@@ -28,6 +33,8 @@ const addCrochet = () => {
   // 초기화
   name.value = "";
   total.value = 0;
+
+  selectRow(newCrochet.id)
 };
 
 const incRow = (id) => {
@@ -43,8 +50,19 @@ const decRow = (id) => {
   saveCrochets();
 };
 
+// 항목 선택
+const selectRow = (id) => {
+  selectedId.value = id;
+};
+
+// 저장
 const saveCrochets = () => {
   localStorage.setItem("crochets", JSON.stringify(crochets.value));
+};
+// 항목 삭제
+const removeRow = (id) => {
+  crochets.value = crochets.value.filter((c) => c.id !== id);
+  saveCrochets();
 };
 
 // 전체 목록초기화
@@ -61,15 +79,52 @@ const resetCrochet = () => {
     <input type="number" v-model="total" placeholder="전체 단수" />
     <button @click="addCrochet">추가</button>
   </div>
+
+  <div v-if="selectedItem" class="selected">
+    <h2>{{ selectedItem.name }}</h2>
+    <p>{{ selectedItem.current }} / {{ selectedItem.total }}</p>
+
+    <button
+      @click="decRow(selectedItem.id)"
+      :disabled="selectedItem.current == 0"
+    >
+      -
+    </button>
+
+    <button
+      @click="incRow(selectedItem.id)"
+      :disabled="selectedItem.current == selectedItem.total"
+    >
+      +
+    </button>
+  </div>
+  <hr />
+
   <ul>
-    <li v-for="p in crochets" :key="p.id">
+    <li
+      v-for="p in crochets"
+      :key="p.id"
+      @click="selectRow(p.id)"
+      :class="{ active: selectedId === p.id }"
+    >
       {{ p.name }} {{ p.current }} / {{ p.total }}
       <button @click="decRow(p.id)" :disabled="p.current == 0">-</button>
       <button @click="incRow(p.id)" :disabled="p.total == p.current">+</button>
+      <p>{{ (p.current / p.total) * 100 + "%" }}</p>
+      <button @click="removeRow(p.id)">삭제</button>
       {{ dayjs(p.createdAt).format("YYYY-MM-DD HH:mm") }}
     </li>
   </ul>
-  <button @click="resetCrochet">초기화</button>
+  <button @click="resetCrochet">전체 초기화</button>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.selected {
+  border: 2px solid red;
+}
+
+.active {
+  background: #f3f3f3;
+  cursor: pointer;
+}
+</style>
